54551763a46a435c1fd6eba3083e5239ee8ade5f
JamesMylan <james.mylan1@education.nsw.gov.au>
2024-05-16 12:57:00 +1000

    Add function to convert from magnitude-angle form to x and y components

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def addVectors(vector1:tuple,*args):
+import math
+from math import radians
+def addVectors(vector1:tuple,*vectors) -> tuple:
+    """
+    Adds two or more vectors and returns the resultant vector
+    """
     resultantVector = vector1
-    for vector in args:
+    for vector in vectors:
         resultantVector = ((resultantVector[0]+vector[0]),(resultantVector[1]+vector[1]))
     return resultantVector
-
+def toXAndY(magnitude,angle) -> tuple: 
+    """
+    Takes a vector in magnitude-angle form and returns it as float x and y components
+    """
+    resultantVector = (magnitude*math.cos(radians(angle)),magnitude*math.sin(radians(angle)))
+    return resultantVector
--- test_main.py
+++ test_main.py
@@ -4,4 +4,6 @@ def test_addVectors():
     assert addVectors((3.23423,1.83),(2.0,2)) == (5.23423,3.83)
     #add multiple vectors
     assert addVectors((1,2),(2,30),(-12,13)) == (-9,45)
+def test_toXAndY():
+    assert toXAndY(3,20) == (3*math.cos(radians(20)),3*math.sin(radians(20)))
 
