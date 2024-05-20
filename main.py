b22c991bac687ee88ddf7c3bd39a6652eb22a47b
JamesMylan <james.mylan1@education.nsw.gov.au>
2024-05-20 11:01:40 +1000

    Create function to output vector in algebraic form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 from math import radians
-def addVectors(vector1:tuple,*vectors) -> tuple:
+def addVectors(vector1:tuple,*vectors:tuple):
     """
     Adds two or more vectors and returns the resultant vector
     """
@@ -8,9 +8,22 @@ def addVectors(vector1:tuple,*vectors) -> tuple:
     for vector in vectors:
         resultantVector = ((resultantVector[0]+vector[0]),(resultantVector[1]+vector[1]))
     return resultantVector
-def toXAndY(magnitude,angle) -> tuple: 
+def toXAndY(magnitude,angle): 
     """
     Takes a vector in magnitude-angle form and returns it as float x and y components
     """
     resultantVector = (magnitude*math.cos(radians(angle)),magnitude*math.sin(radians(angle)))
     return resultantVector
+def toAlgebraicForm(vector:tuple):
+    """
+    Takes a vector in component form and outputs algebraic form as a string
+    """
+    if isinstance(vector[0],float):
+        xComponent = f'{float(vector[0])}i'
+    else:
+        xComponent = f'{int(vector[0])}i'
+    if isinstance(vector[1],float):
+        yComponent = f'{float(vector[1]):+}j'
+    else:
+        yComponent = f'{int(vector[1]):+}j'
+    return f'{xComponent} {yComponent}'
--- test_main.py
+++ test_main.py
@@ -6,4 +6,7 @@ def test_addVectors():
     assert addVectors((1,2),(2,30),(-12,13)) == (-9,45)
 def test_toXAndY():
     assert toXAndY(3,20) == (3*math.cos(radians(20)),3*math.sin(radians(20)))
+def test_toAlgebraicForm():
+    assert toAlgebraicForm((5,-6)) == "5i -6j"
+    assert toAlgebraicForm((5.2,2.77723)) == "5.2i +2.77723j"
 
